f7ab92b774d65ea78538bedb5eef059441001b7f
Ahmed Nouira <ahmnouira@gmail.com>
2021-07-12 20:11:25 +0100

    feat: add export functionality

---
+++ src/components/CSVDownlaoder.tsx
@@ -0,0 +1,30 @@
+import { IonItem, IonButton } from '@ionic/react'
+
+import {CSVDownloader as ReactCSVDownloader, jsonToCSV}  from  "react-papaparse"
+import { data } from '../datasource/data'
+
+type CSVDownloaderProps =  {
+  data: any[]
+}
+
+export const CSVDownloader  = ({}: CSVDownloaderProps) => {
+    return (
+      <ReactCSVDownloader
+        data={jsonToCSV(data)}
+        type="button"
+      
+        filename={'data'}
+        bom={true}
+      >
+
+      <IonItem lines="none">
+      <IonButton color="primary" size="large" slot="end">
+      Export
+        </IonButton>
+    </IonItem>
+     
+
+        </ReactCSVDownloader>
+       
+    )
+  }
--- src/components/TableData.tsx
+++ src/components/TableData.tsx
@@ -1,17 +1,12 @@
-import {IonGrid, IonRow, IonCol, IonButton, IonItem, IonText} from '@ionic/react'
-import { Icon } from 'ionicons/dist/types/components/icon/icon';
+import {IonGrid, IonRow, IonCol} from '@ionic/react'
 import React from 'react';
 import { data } from '../datasource/data';
-import { convertToCSV, saveAsCSV } from '../services/file.service';
+import { CSVDownloader } from './CSVDownlaoder';
 import './TableData.css';
 
 type TableDataProps =  { }
 
-
 export const TableData: React.FC<TableDataProps> = () => {
-
-
-  const [csv, setCSV] = React.useState('')
 
 
   const renderHeaders = data.map((d) => {
@@ -26,39 +21,21 @@ export const TableData: React.FC<TableDataProps> = () => {
       return <IonCol key={`${d.type}-${row}`}>{data[typeIndex].values[row]}</IonCol>
     })}
     </IonRow>
-
-
   )
   })
 
-  const handlePress =  async () => {
-  const csv =  convertToCSV(data)
-  setCSV(csv)
-  console.log('csv', csv)
-  await saveAsCSV(csv)
-
-  }
 
   return (
    <React.Fragment>
-
   <IonGrid class="ion-margin">
-    
     <IonRow color="red">
       {renderHeaders}
     </IonRow>
     {renderRows}
-   
   </IonGrid>
-
-  <IonItem lines="none">
-    <IonButton color="primary" size="large" slot="end" onClick={handlePress}>
-      export
-    </IonButton>
-  </IonItem>
-
-  {csv ? <IonText>{csv}</IonText> : null}
-  </React.Fragment>
+      <CSVDownloader data={data} />
+  
+ </React.Fragment>
   );
 };
 
