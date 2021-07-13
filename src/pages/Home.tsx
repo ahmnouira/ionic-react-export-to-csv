c58ce12e0cd4a48c6c6fdd0f3dc8154c44e022ed
Ahmed Nouira <ahmnouira@gmail.com>
2021-07-13 23:27:28 +0100

    feat: open and save the exported csv on ios

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,26 @@
 import * as React from 'react'
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButtons, IonButton, IonInput, IonGrid, IonRadio, IonRow } from '@ionic/react'
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonButtons,
+  IonButton,
+  IonInput,
+  IonGrid,
+  IonRadio,
+  IonRow,
+} from '@ionic/react'
 import './Home.css'
 import { isPlatform } from '@ionic/react'
 import { jsonToCSV, readString } from 'react-papaparse'
 import { data } from '../datasource/data'
-import { saveAsCSV } from '../services/file.service'
-
+import { readFile, resolveFile, saveAsCSV } from '../services/file.service'
+import { openCSVFile } from '../services/fileOpener.service'
 
 type webCSVType = {
-  isWeb: boolean,
+  isWeb: boolean
   data?: any
 }
 
@@ -17,8 +29,8 @@ const Home: React.FC = () => {
   const [headerRow, setHeaderRow] = React.useState<any[]>([])
 
   const [webCSV, setWebCSV] = React.useState<webCSVType>({
-      isWeb: false,
-  }) 
+    isWeb: false,
+  })
 
   React.useEffect(() => {
     loadCSV()
@@ -47,31 +59,37 @@ const Home: React.FC = () => {
     })
 
     let blob = new Blob([csv], {
-      type: "text/csv"
+      type: 'text/csv',
     })
 
     if (isPlatform('cordova')) {
-      const saved = await saveAsCSV(blob)
+      try {
+        const saved = await saveAsCSV(blob)
+        if (saved) {
+          console.log('saved::', saved.nativeURL)
+          const openfile = await openCSVFile(saved.nativeURL)
+          console.log('opned', openfile)
+        }
+      } catch (error) {
+        console.error(error)
+      }
+
       /*
       then (res)
       this.socialSharing.share(null, null, res.nativeURL, null).then(e =>{
       */
     } else {
       // Dummy implementation for Desktop download purpose
-      
-     
+
       setWebCSV({
         isWeb: true,
-        data:   URL.createObjectURL(blob)
+        data: URL.createObjectURL(blob),
       })
     }
   }
 
   const renderHeader = headerRow.map((header, index) => {
-    return (
-      <th key={index}>{ header}
-      </th>
-    )
+    return <th key={index}>{header}</th>
   })
 
   const reanderRows = csvData.map((row: any[], rowIndex) => {
@@ -80,7 +98,7 @@ const Home: React.FC = () => {
         {row.map((data, dataIndex) => {
           return (
             <td key={dataIndex} className='data-col'>
-              <IonInput type='text' value={csvData[rowIndex][dataIndex]}  />
+              <IonInput type='text' value={csvData[rowIndex][dataIndex]} />
             </td>
           )
         })}
@@ -92,36 +110,28 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar color='primary'>
-          <IonTitle>
-            
-          </IonTitle>
+          <IonTitle></IonTitle>
           <IonButtons slot='start'>
             <IonButton onClick={handleExport}>Export</IonButton>
           </IonButtons>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen scrollX>
-
         <IonGrid>
-
-        <IonRow>
-
-       
-        <table className='styled-table'>
-          <thead>
-            <tr>         
-          {renderHeader}
-          </tr>
-          </thead>
-          <tbody>
-
-        
-          {reanderRows}
-          </tbody>
-        </table>
-        </IonRow>
+          <IonRow>
+            <table className='styled-table'>
+              <thead>
+                <tr>{renderHeader}</tr>
+              </thead>
+              <tbody>{reanderRows}</tbody>
+            </table>
+          </IonRow>
         </IonGrid>
-        {webCSV && webCSV.isWeb && webCSV.data ? <a href={webCSV.data} download={'data.csv'} style={{fontSize: 34, color: 'red'}}>Download data</a> : null}
+        {webCSV && webCSV.isWeb && webCSV.data ? (
+          <a href={webCSV.data} download={'data.csv'} style={{ fontSize: 34, color: 'red' }}>
+            Download data
+          </a>
+        ) : null}
       </IonContent>
     </IonPage>
   )
--- src/services/file.service.ts
+++ src/services/file.service.ts
@@ -2,21 +2,15 @@ import { File } from '@ionic-native/file'
 
 export async function saveAsCSV(csv: Blob) {
   var fileName: any = 'data.csv'
-
-  File.writeFile(File.dataDirectory, fileName, csv, {
+  return await File.writeFile(File.dataDirectory, fileName, csv, {
     replace: true,
-    
   })
-    .then((_) => {
-      alert(File.dataDirectory)
-    })
-    .catch((err) => {
-      File.writeExistingFile(File.dataDirectory, fileName, csv)
-        .then((_) => {
-          alert('Success ;-)' + File.dataDirectory)
-        })
-        .catch((err) => {
-          alert('Failure' + File.dataDirectory)
-        })
-    })
+}
+
+export async function resolveFile(path: string) {
+  return await File.resolveLocalFilesystemUrl(path)
+}
+
+export async function readFile(path: string) {
+  return await File.readAsDataURL(path, 'data.csv')
 }
